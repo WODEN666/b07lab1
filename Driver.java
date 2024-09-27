f24a4ac5dcb47ceebccc08537ff69d1a64c26bec
Shin <yxt@xt-ydeMacBook-Air.local>
2024-09-27 16:23:51 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,15 @@
 public class Driver {
     public static void main(String [] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+        double [] coeff = {6, -2, 5};
+        int [] expo = {0, 1, 3};
+        Polynomial p1 = new  Polynomial(coeff, expo);
+        Polynomial p2 = new  Polynomial("Desktop/text.txt");
+
+        Polynomial product = p1.multiply(p2);
+        product.saveToFile("Desktop/p1.txt");
+        System.out.println("p2(0.1)" + p2.evaluate(0.1));
+
+
+
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,5 +1,8 @@
+import java.io.*;
+
 public class Polynomial{
     double [] coeff;
+    int [] expo;
 
     Polynomial(){
         coeff = new double[1];
@@ -7,11 +10,93 @@ public class Polynomial{
 
     }
 
-    Polynomial(double [] coeff){
+    Polynomial(double [] coeff, int [] expo){
         this.coeff = coeff;
-
+        this.expo = expo;
     }
     
+    Polynomial(File file){
+        
+        try{
+            Filereader filereader = new Filereader(file);
+            BufferedReader reader = new BufferedReader(filereader);
+
+            String line = reader.readline();
+
+          
+
+            String [] terms = line.split("(?=[+-])");
+            coeff = new coeff[terms.length];
+            expo = new expo[terms.length];
+
+            for (int i = 0; i < terms.length; i++){
+                String term = terms[i];
+                if(term.contains("x")){
+                    String [] substring = term.split("x");
+                    coeff[i] = parseDouble(subtring[0]);
+                    expo[i] = parseInt(subtring[1]);
+                }
+                else{
+                    coeff[i] = parseDouble(term);
+                    expo[i] = 0;
+                }
+            }
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        coeff = new double[str.length];
+        expo = new int[str.length];
+
+
+    }
+
+    Polynomial multiply(Polynomial p2){
+        result_coeff = new double[this.coeff.length * p2.coeff.length];
+        result_expo = new int[this.expo.length * p2.expo.length];
+
+        int index = 0;
+        for (int i = 0; i < this.coeff.length; i++){
+            for(int j = 0; j < p2.coeff.length; j++){
+                result_coeff[index] = this.coeff[i] * p2.coeff[j];
+                result_expo[index] = this.expo[i] + p2.expo[j];
+                index++;
+            }
+        }
+
+        return new Polynomial(result_coeff, result_expo);
+    }
+
+    void saveToFile(String fileName){
+        try{
+            Filewriter filewriter = new FileWriter(fileName);
+            BufferedWriter writer = new BufferedWriter(filewriter);
+
+            for(int i = 0;i < coeff.length; i++){
+                double coefficient = coeff[i];
+                int exponent = expo[i];
+                if(i == 0){
+                    writer.write(coefficent);
+                    if(exponent != 0){
+                        writer.write("x^" + exponent);
+                    }
+                    
+                }
+                else{
+                    if (coefficient > 0){
+                        writer.write("+");
+                    }
+                    writer.write(coefficient + "x^" + exponent);
+                }
+
+            }
+        }
+
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
     Polynomial add(Polynomial poly){
         int maxLength = Math.max(this.coeff.length, poly.coeff.length);
         double [] result = new double[maxLength];
@@ -27,11 +112,12 @@ public class Polynomial{
 
     }
 
+
     double evaluate(double x){
         double result = 0;
 
         for (int i = 0; i < coeff.length; i++)
-            result += coeff[i] * Math.pow(x, i);
+            result += coeff[i] * Math.pow(x, expo[i]);
         
 
         return result;
@@ -41,7 +127,7 @@ public class Polynomial{
         double result = 0;
 
         for (int i = 0; i < coeff.length; i++)
-            result += coeff[i] * Math.pow(x, i);
+            result += coeff[i] * Math.pow(x, expo[i]);
 
         if (result == 0)
             return true;
